1b67c6232896ac353c21e85a5d90bdac6942f537
laibagohar923 <149240308+laibagohar923@users.noreply.github.com>
2024-04-11 10:51:49 -0700

    Add files via upload

---
+++ Qno.14 Guest List/app.js
@@ -0,0 +1,9 @@
+"use strict";
+// Question no.14 Guest List
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.guest_list = void 0;
+let guest_list = [`Laiba`, `Iqra`, `Fabiha`, `Maryam`];
+exports.guest_list = guest_list;
+for (let i = 0; i < guest_list.length; i++) {
+    console.log(`Respected sir/Madam` + guest_list[i] + ',\nWe invited you on dinner tomorrow.\n\nThank You');
+}
---
+++ Qno.14 Guest List/app.ts
@@ -0,0 +1,7 @@
+// Question no.14 Guest List
+
+let guest_list : string [] = [`Laiba`, `Iqra`, `Fabiha`, `Maryam`];
+for(let i=0; i<guest_list.length; i++){
+    console.log(`Respected sir/Madam` + guest_list[i] + ',\nWe invited you on dinner tomorrow.\n\nThank You')
+}
+    export{guest_list}
