6959952995d0046962977a78a03c1c1e8f37d74b
laibagohar923 <149240308+laibagohar923@users.noreply.github.com>
2024-04-11 10:18:59 -0700

    Add files via upload

---
+++ Qno.5 Famouse quote 2/app.ts
@@ -0,0 +1,7 @@
+// Question no: 5 famouse quote 2
+
+let quote: string = "A person who never made a mistake never tried anything new";
+let famouse_person = "Albert Einestein";
+let message = `${famouse_person} once said, ${quote}`;
+
+console.log (message);
