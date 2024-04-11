e79c28c2598318d2b77c778d1bee2f02dfd6dbb0
laibagohar923 <149240308+laibagohar923@users.noreply.github.com>
2024-04-11 11:22:45 -0700

    Add files via upload

---
+++ Qno.22 Conditional Tests/app.ts
@@ -0,0 +1,77 @@
+// Question no.22 conditional tests
+
+let car: string = 'subaru';
+
+// Test 1 : Equality comprassion (True)
+console.log("Is car == 'subaru'? I predicat true.");
+console.log(car == 'subara'); //True
+
+// Test 2 : Equality comprassion (True)
+console.log("Is car === 'subaru'? I predicat true.");
+console.log(car === 'subara'); //True
+
+// Test 3 : Inequality comprassion (Falsa)
+console.log("Is car != 'subaru'? I predicat falsa.");
+console.log(car != 'subara'); //Falsa
+
+// Test 4 :  strict inequality comprassion (Falsa)
+console.log("Is car !== 'subaru'? I predicat falsa.");
+console.log(car !== 'subara'); //False
+
+// Test 5 : less then comprassion (Falsa)
+console.log("Is car < 'subaru'? I predicat falsa.");
+console.log(car < 'subara'); //False(lexicographic compression)
+
+// Test 6 : Greater then comprassion (True)
+console.log("Is car > 'subaru'? I predicat true.");
+console.log(car > 'subara'); //True
+
+// Test 7 : Less then or equal comprassion (True)
+console.log("Is car <= 'subaru'? I predicat true.");
+console.log(car <= 'subara'); //True
+
+// // Test 8 : Greater then or comprassion (True)
+console.log("Is car >= 'subaru'? I predicat true.");
+console.log(car >= 'subara'); //True
+
+// // Test 9 : Cheaking truthniess (True)
+console.log("Is car? 'subaru'? I predicat true.");
+console.log(car); //True(non_empty string is truthy)
+
+// // Test 10 : Cheaking falsiness (True)
+ console.log("Is !car ? 'subaru'? I predicat falsa.");
+console.log(!car); //Fasle(negation of a  truthy value)
+
+
+
+
+
+
+
+
+
+
+// let car: string = 'subaru';
+// let age: number = 20;
+// let numbers: number[] = [3, 5, 7, 9, 10];
+
+// // string test..
+
+// // True test
+// console.log("Is car == 'subaru'? I predicat False.");
+// console.log(car == 'subara'); //True
+
+
+// // Falsa test
+// console.log("Is car != 'subaru'? I predicat True.");
+// console.log(car != 'subara'); //Falsa
+
+
+
+
+
+
+
+
+
+
