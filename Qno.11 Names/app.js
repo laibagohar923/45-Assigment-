d340a5e3f89892e9760ecc3eff3ec629e7f407ec
laibagohar923 <149240308+laibagohar923@users.noreply.github.com>
2024-04-11 10:42:14 -0700

    Add files via upload

---
+++ Qno.11 Names/app.js
@@ -0,0 +1,5 @@
+// Question No.11 Names
+var members = ["Ayesha", "Eshal", "Ahmad", "Huzaifa", "Nehal"];
+for (var i = 0; i < members.length; i++) {
+    console.log(members[i]);
+}
---
+++ Qno.11 Names/app.ts
@@ -0,0 +1,6 @@
+// Question No.11 Names
+
+let members: string[] = [ `Ayesha`, `Eshal`, `Ahmad`, `Huzaifa`, `Nehal`];
+for( let i=0; i<members.length; i++){
+    console.log(members[i]);
+}
