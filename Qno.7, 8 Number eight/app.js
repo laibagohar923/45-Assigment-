609534bbc05e0bf5bb0ce739ea059e416232a3ce
laibagohar923 <149240308+laibagohar923@users.noreply.github.com>
2024-04-11 10:23:59 -0700

    Add files via upload

---
+++ Qno.7, 8 Number eight/app.js
@@ -0,0 +1,5 @@
+// Question no. 7,8 Eight Numbar
+console.log(5 + 3);
+console.log(11 - 3);
+console.log(4 * 2);
+console.log(16 / 2);
---
+++ Qno.7, 8 Number eight/app.ts
@@ -0,0 +1,6 @@
+// Question no. 7,8 Eight Numbar
+
+console.log(5 + 3);
+console.log(11 - 3);
+console.log(4 * 2);
+console.log(16 / 2);
