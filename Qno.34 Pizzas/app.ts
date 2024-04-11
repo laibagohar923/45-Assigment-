8c218020db2d21ccedab9324f0529c30c6cc3b17
laibagohar923 <149240308+laibagohar923@users.noreply.github.com>
2024-04-11 12:06:53 -0700

    Add files via upload

---
+++ Qno.34 Pizzas/app.ts
@@ -0,0 +1,16 @@
+// Question no 34 PIZZAS
+
+
+let myPizza = ["cheeze pizza" , "pepperoni pizza", "vegeterian pizza"];
+
+//print only names of pizza
+for (let i = 0; i < myPizza.length; i++){
+    console.log(myPizza[i]);
+}
+// printing names and sentence/message
+for (let i = 0; i < myPizza.length; i++){
+    console.log(`i like to eat ${myPizza[i]}`);
+}
+console.log (
+    `\nI really like to eat pizzas. pizza comes in a verity of flavors and toopings, allowing individuals to customize it to their linking`
+)
