25509fce3edd0803ed69550a6605d5406ed7fcc3
laibagohar923 <149240308+laibagohar923@users.noreply.github.com>
2024-04-11 11:45:27 -0700

    Add files via upload

---
+++ Qno.29 Favorite Fruit/app.js
@@ -0,0 +1,24 @@
+"use strict";
+// Question no. 29 Favorite Fruit 
+// Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+// • Make a array of your three favorite fruits and call it favorite_fruits.
+let favorite_fruits = ['Apple', 'Mango', 'Banana'];
+if (favorite_fruits.includes('Apple')) {
+    console.log('I like Apple!');
+}
+// • Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+if (favorite_fruits.includes('Mango')) {
+    console.log('I like Mango!');
+}
+if (favorite_fruits.includes('Banana')) {
+    console.log('I like Banana!');
+}
+if (favorite_fruits.includes('Orange')) {
+    console.log('I like Orange!');
+}
+if (favorite_fruits.includes('Grapes')) {
+    console.log('I like Grapes!');
+}
+if (favorite_fruits.includes('Papaya')) {
+    console.log('I like Papaya!');
+}
