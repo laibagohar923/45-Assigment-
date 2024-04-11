650cadbb37877b495ef6ab71c0717fd38704b243
laibagohar923 <149240308+laibagohar923@users.noreply.github.com>
2024-04-11 10:48:51 -0700

    Add files via upload

---
+++ Qno.13 Your Own Array/app.js
@@ -0,0 +1,6 @@
+"use strict";
+// Question no.13 Your Own Array
+let transportaion = [`civic`, `corolla`, `marcedes-benz`, `lamborghini`, `Bike`];
+for (let i = 0; i < transportaion.length; i++) {
+    console.log(`I would like to a own ` + transportaion[i]);
+}
---
+++ Qno.13 Your Own Array/app.ts
@@ -0,0 +1,7 @@
+// Question no.13 Your Own Array
+
+let transportaion : string [] = [`civic`, `corolla`, `marcedes-benz`, `lamborghini` , `Bike`]
+for(let i=0; i<transportaion.length; i++){
+    console.log(`I would like to a own ` + transportaion[i]);
+  
+}
