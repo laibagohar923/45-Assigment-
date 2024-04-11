4b5a5e36c837797e4be6da65b0b18a8d4cbd16f1
laibagohar923 <149240308+laibagohar923@users.noreply.github.com>
2024-04-11 10:10:21 -0700

    Add files via upload

---
+++ Qno.2 Personal Message/app.js
@@ -0,0 +1,4 @@
+"use strict";
+// Question no. 2 Personal message
+let personName = "ERIC";
+console.log(`Hellow ${personName}, would you like to same python today?`);
---
+++ Qno.2 Personal Message/app.ts
@@ -0,0 +1,4 @@
+// Question no. 2 Personal message
+
+let personName: string= "ERIC";
+console.log(`Hellow ${personName}, would you like to same python today?`);
