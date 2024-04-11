77e5d47de999c5e77b88085874f011a96c8888fa
laibagohar923 <149240308+laibagohar923@users.noreply.github.com>
2024-04-11 11:48:17 -0700

    Add files via upload

---
+++ Qno.30 Hello admin/app.ts
@@ -0,0 +1,12 @@
+// Question no.30 Hello admin
+let users: string [] = ['sana','babar', 'admin', 'laiba', 'ahmad']
+
+for (let user of users){
+    if(user === "admin"){
+        console.log(" Hello admin, would you like to see a status report?")
+    }
+
+else{
+    console.log(`Hello ${users}, thank you for logging in again`)
+}
+}
