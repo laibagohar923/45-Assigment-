651aafcc281d76f73a1f8b2c351b7ae9fa3a7891
laibagohar923 <149240308+laibagohar923@users.noreply.github.com>
2024-04-11 11:12:31 -0700

    Add files via upload

---
+++ Qno.19 Dinner Guests/app.js
@@ -0,0 +1,6 @@
+"use strict";
+// Question no.  19 Dinner guests
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = require("../Qno.14 Guest List/app");
+console.log(`n\ PRINTING NUMBER  OF GUEST INVITED`);
+console.log(` we had finally invited ${app_1.guest_list.length} from Qno.14`);
---
+++ Qno.19 Dinner Guests/app.ts
@@ -0,0 +1,7 @@
+// Question no.  19 Dinner guests
+
+
+import { Console } from 'console'
+import{ guest_list } from '../Qno.14 Guest List/app'
+console.log(`n\ PRINTING NUMBER  OF GUEST INVITED`)
+console.log(` we had finally invited ${guest_list.length} from Qno.14`)
