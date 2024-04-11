c95f8e60acbaa7e4e45bba478db0f24aba6fd745
laibagohar923 <149240308+laibagohar923@users.noreply.github.com>
2024-04-11 11:33:25 -0700

    Add files via upload

---
+++ Qno.27 Alien Colors #3/app.js
@@ -0,0 +1,48 @@
+"use strict";
+// Question no. 27 Alien colors #3
+let alien_color = "Green";
+// • If the alien is green, print a message that the player earned 5 points.
+if (alien_color == "Green") {
+    console.log("the player just earned 5 points.");
+}
+// • If the alien is yellow, print a message that the player earned 10 points.
+else if (alien_color == "Yellow") {
+    console.log("the player just earned 10 points.");
+}
+// • If the alien is red, print a message that the player earned 15 points.
+else if (alien_color == "Red") {
+    console.log("the player just earned 15 points.");
+}
+else {
+    console.log("please select right color");
+}
+// • Write three versions of this program, making sure
+// each message is printed for the appropriate color alien.
+// version 2
+alien_color = "Yellow";
+if (alien_color == "Green") {
+    console.log("the player just earned 5 points.");
+}
+else if (alien_color == "Yellow") {
+    console.log("the player just earned 10 points.");
+}
+else if (alien_color == "Red") {
+    console.log("the player just earned 15 points.");
+}
+else {
+    console.log("please select right color");
+}
+// version 3
+alien_color = "Green";
+if (alien_color == "Green") {
+    console.log("the player just earned 5 points.");
+}
+else if (alien_color == "Yellow") {
+    console.log("the player just earned 10 points.");
+}
+else if (alien_color == "Red") {
+    console.log("the player just earned 15 points.");
+}
+else {
+    console.log("please select right color");
+}
