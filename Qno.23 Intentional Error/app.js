c0d839c83563d1c661ba7252d1e87210f2a475e8
laibagohar923 <149240308+laibagohar923@users.noreply.github.com>
2024-04-11 11:25:22 -0700

    Add files via upload

---
+++ Qno.23 Intentional Error/app.js
@@ -0,0 +1,7 @@
+"use strict";
+// Question no. 23 Intentional Error
+let array = ['Ahmad', 'Talha', 'Hassan', 'Ali', 'Saim'];
+console.log(array[4]);
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
---
+++ Qno.23 Intentional Error/app.ts
@@ -0,0 +1,7 @@
+// Question no. 23 Intentional Error
+
+let array: ( string | number ) [] = ['Ahmad', 'Talha','Hassan' , 'Ali' , 'Saim' ]
+console.log(array[4]);
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
