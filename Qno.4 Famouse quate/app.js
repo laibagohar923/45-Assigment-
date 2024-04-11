7e01e921543fc39f56bd911f685eb79cf840099d
laibagohar923 <149240308+laibagohar923@users.noreply.github.com>
2024-04-11 10:16:54 -0700

    Add files via upload

---
+++ Qno.4 Famouse quate/app.js
@@ -0,0 +1,5 @@
+"use strict";
+// Question no: 04 Famouse quate.
+let qoute = "A person who never made a mistake never tried anything new";
+let author = "Albert Einstein";
+console.log(`${author} once said, ${qoute}`);
---
+++ Qno.4 Famouse quate/app.ts
@@ -0,0 +1,5 @@
+// Question no: 04 Famouse quate.
+
+let qoute: string= "A person who never made a mistake never tried anything new";
+let author: string= "Albert Einstein"
+console.log(`${author} once said, ${qoute}`);
