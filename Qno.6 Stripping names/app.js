fe8133dd817efcc7ae0b46a53d1f6f9459102016
laibagohar923 <149240308+laibagohar923@users.noreply.github.com>
2024-04-11 10:21:18 -0700

    Add files via upload

---
+++ Qno.6 Stripping names/app.js
@@ -0,0 +1,6 @@
+"use strict";
+// Question no: 6 stripping name
+let personName = `\n\t SIR AMEEN ALAM \t\n`;
+console.log(personName);
+let stripped = personName.trim();
+console.log(stripped);
---
+++ Qno.6 Stripping names/app.ts
@@ -0,0 +1,7 @@
+// Question no: 6 stripping name
+
+let personName: string = `\n\t SIR AMEEN ALAM \t\n`;
+console.log(personName);
+
+let stripped: string = personName.trim();
+console.log(stripped);
