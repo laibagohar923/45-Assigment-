d76d14f4ded418e69b04ff4d8dba09ec6803d0f7
laibagohar923 <149240308+laibagohar923@users.noreply.github.com>
2024-04-11 11:03:02 -0700

    Add files via upload

---
+++ Qno.17 Shrinking guest list/app.ts
@@ -0,0 +1,28 @@
+// Question no 17. Shrinking guest list
+
+let guest_list : string [] = [`Laiba`, `Iqra`, `Simra`, `Maryam`];
+// for (let i=0; i<guest_list.length; i++){
+//     console.log('Respected sir/Madam' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\nThank you\n')
+// }
+let not_present : string = 'Ahmad';
+let new_guest : string = 'Zubair';
+guest_list[1] = new_guest;
+// for (let i=0; i<guest_list.length; i++){
+//     console.log ('Respected sir/Madam' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\nThank you\n')
+// }
+// console.log (`Mr. ${not_present} will not coming tomorrow on dinner.`)
+
+guest_list.unshift('Shaheen', 'Sarfaraz', 'Abdullah')
+// for (let i=0; i<guest_list.length; i++){
+//     console.log ('Respected sir/Madam' + guest_list[i] + ',\nWe invited you on dinner tomorrow. We found big table so we decided to invite 3 more guest\nThank you\n')
+// }
+console.log('\nUnfortunately we can arrange big table , only two people allowed')
+while(guest_list.length>2){
+    let remove_guest = guest_list.pop();
+    console.log(`Sorry Sir/Madam. ${remove_guest} you are not invited for dinner.`);
+}
+for(let i=0; i<guest_list.length; i++){
+    console.log(`Recpected Sir/Madam` + guest_list[i] + `,\nYes you are still invited on tomorrow dinner\nThank you\n`)
+}
+guest_list.splice(0,2)
+console.log(guest_list)
