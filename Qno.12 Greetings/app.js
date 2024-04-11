26232128507ded929ab04bb7705b1bdc155e4ec1
laibagohar923 <149240308+laibagohar923@users.noreply.github.com>
2024-04-11 10:45:51 -0700

    Add files via upload

---
+++ Qno.12 Greetings/app.js
@@ -0,0 +1,7 @@
+"use strict";
+// Questin no. 12 Greetings
+let members = [`Ayesha`, `Eshal`, `Ahmad`, `Huzaifa`, `Nehal`];
+let message = 'I see you tomorrow: ';
+for (let i = 0; i < members.length; i++) {
+    console.log(message + members[i]);
+}
---
+++ Qno.12 Greetings/app.ts
@@ -0,0 +1,7 @@
+// Questin no. 12 Greetings
+
+let members: string[] =[`Ayesha`, `Eshal`, `Ahmad`, `Huzaifa`, `Nehal`];
+let message: string='I see you tomorrow: ';
+for(let i=0; i<members.length; i++){
+    console.log(message + members[i]);
+}
