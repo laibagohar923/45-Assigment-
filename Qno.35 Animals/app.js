b59d291512e3921207b4ad703be3fb9e143a3979
Laiba Gohar <149240308+laibagohar923@users.noreply.github.com>
2024-04-13 12:14:55 -0700

    Add files via upload

---
+++ Qno.35 Animals/app.js
@@ -0,0 +1,7 @@
+"use strict";
+// Question no. 35 Animals
+let animals = ["dog", "elephent", "rabbit "];
+animals.forEach(animals => {
+    console.log(`A ${animals} would make a great pet`);
+});
+console.log("Any of these animals would make a great pet!");
