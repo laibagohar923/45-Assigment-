56d0c49eb911855dcb1cb4099f2c761e4c8aa56e
Laiba Gohar <149240308+laibagohar923@users.noreply.github.com>
2024-04-13 12:11:46 -0700

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// Question no. 35 Animals
-let animals = ["dog", "elephent", "rabbit "];
-animals.forEach(animals => {
-    console.log(`A ${animals} would make a great pet`);
-});
-console.log("Any of these animals would make a great pet!");
