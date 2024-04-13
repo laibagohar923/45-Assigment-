54e03aff97af225bdfa5bf66ae8e8de3c9d7d777
Laiba Gohar <149240308+laibagohar923@users.noreply.github.com>
2024-04-13 12:17:10 -0700

    Add files via upload

---
+++ Qno.36 T-Shirt/app.js
@@ -0,0 +1,7 @@
+"use strict";
+// Question no. 36 T-Shirt
+function makeShirt(size, text) {
+    console.log(`you oder a ${size} shirt that says ${text}`);
+}
+makeShirt('Large', '"I love typescript"');
+makeShirt('Medium', '"I need a big shirt"');
---
+++ Qno.36 T-Shirt/app.ts
@@ -0,0 +1,9 @@
+// Question no. 36 T-Shirt
+
+function makeShirt(size: string, text: string): void{
+    console.log(`you oder a ${size} shirt that says ${text}`)
+}
+
+makeShirt('Large','"I love typescript"')
+
+makeShirt('Medium','"I need a big shirt"')
