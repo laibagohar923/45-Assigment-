7ea28dcda6987ef4d02ca47b6d83ca16fa787ac4
Laiba Gohar <149240308+laibagohar923@users.noreply.github.com>
2024-04-13 12:29:15 -0700

    Add files via upload

---
+++ Qno. 39 City Names/app.ts
@@ -0,0 +1,13 @@
+// Question no. 30 Cites Names
+ 
+function cityCountry(city : string, country : string): string {
+    return `${city}, ${country}`
+}
+
+let c1 = cityCountry('lahore', 'pakistan')
+let c2 = cityCountry('tukyo', 'japan')
+let c3 = cityCountry('paris', 'france')
+
+console.log(c1)
+console.log(c2)
+console.log(c3)
