8d28ebbe67c9aaf5183d21911f88d1df71464327
Laiba Gohar <149240308+laibagohar923@users.noreply.github.com>
2024-04-13 12:11:58 -0700

    Delete app.ts

--- app.ts
+++
@@ -1,9 +0,0 @@
-// Question no. 35 Animals
-
-let animals: string [] = ["dog", "elephent", "rabbit "];
-
-animals.forEach(animals => {
-    console.log(`A ${animals} would make a great pet`);
-
-})
-console.log("Any of these animals would make a great pet!")
